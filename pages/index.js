783f242dcb535a5236ba3d64de381d1c72937d15
Jared DiScipio <36304698+p0pd0c@users.noreply.github.com>
2021-06-14 13:43:48 -0400

    Add example request code

--- pages/index.js
+++ pages/index.js
@@ -5,5 +5,13 @@ export default function Index() {
       domain and make a POST / GET / OPTIONS request to <b>/api/cors</b>. Using
       a different method from those mentioned will be blocked by CORS
     </p>
+    <p>fetch('https://api-routes-cors-green-iota.vercel.app/api/cors', {method: "PATCH"})
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            console.log(data);
+          });
+  </p>
   )
 }
